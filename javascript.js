503e9f3c1b70fe6898f34f5983812503013986cf
JavarnTr <83565578+JavarnTr@users.noreply.github.com>
2022-10-18 17:01:25 +1300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -2,6 +2,36 @@
 let proxy = 'https://cors-anywhere.herokuapp.com/' ;
 let url = 'http://danieldangs.com/itwd6408/json/faqs.json';
 
+//---------------------------------- Home Page ----------------------------------//
+
+//Change service fee depending on whether the user has warranty or not
+	$("#gridCheck1").change(function() {
+		if(this.checked) {
+			$("#serviceFee").val("0.00")
+		} else {
+			$("#serviceFee").val("85.00")
+		}
+	})
+
+	let courtesyList = [{item: 'iPhone', bond: 275},
+		{item: 'otherPhone', bond: 100},
+		{item: 'charger', bond: 30}
+	];
+
+	let appState = {customerType: 'customer',
+		courtesyPhone: {item: 'none', bond: 0 },//Allow to borrow ONLY 1 phone
+		courtesyCharger: {item: 'none', bond: 0}//Allow to borrow ONLY 1 charger
+ 	}; 
+
+	$("#addBtn").click(function(){
+		let selectedItemText = $("#itemList").find("selected").text();
+		let selectedItemValue = $("#itemList").find("").val();
+		let selectedItemBond = courtesyList.find(foundItem => foundItem.item.toLowerCase() == selectedItemValue.toLowerCase()).bond;
+	})
+
+//---------------------------------- FAQ Page ----------------------------------//
+
+//Get the json data for the FAQ page
 //Jquery function "getJSON"
 $.getJSON(
 	proxy + url,//Send request to the url to get the JSON file 
@@ -20,6 +50,7 @@ $.getJSON(
 	}//JSON file returned in "data"
 );
 
+//Search box to filter the FAQ
 $('#search-box').on('keyup', function(){
 	let keywords = $(this).val().toLowerCase();
 	//Loop through the Q&A to find what was searched
@@ -28,3 +59,23 @@ $('#search-box').on('keyup', function(){
 		$(this).toggle($(this).html().toLowerCase().indexOf(keywords) > -1);
 	});
 });
+
+//---------------------------------- Extension Page ----------------------------------//
+
+//Accordian menu
+$('.content-demo-area div').hide();
+//Loop through all buttons
+//Hide all content sections and display only the according areas
+//Highlight the button 
+$('.btn-demo-area button').on('click', function(){
+	//Set all button backgrounds to white
+	$('.btn-demo-area button').css('background-color', 'white');
+	//Change background color
+	$(this).css('background-color', 'orange');
+	//Hide all content areas
+	$('.content-demo-area div').hide();
+	//Show only the content area matching to the clicked button
+	$('.content-demo-area div').eq($(this).index()).show(1000);
+});
+
+
