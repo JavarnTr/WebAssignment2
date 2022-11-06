1a0506c82e396c9f9ce53353b9c2e406d39692bf
JavarnTr <83565578+JavarnTr@users.noreply.github.com>
2022-11-07 11:54:37 +1300

    file upload

--- javascript.js
+++ javascript.js
@@ -264,6 +264,16 @@ $("#content-button-5").on("click", function () {
     $("#content-area-5").show(1000);
 });
 
+//Get the image from the image upload and display it in a div
+const image_input = document.querySelector("#image-input");
+image_input.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
 
 
 //---------------------------------- Repair Page ----------------------------------//
